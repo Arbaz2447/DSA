4a1ec960ceb22bd5001dc2cb8cc1dd74793fe0d5
Arbaz2447 <127314616+Arbaz2447@users.noreply.github.com>
2025-07-19 03:18:40 +0530

    Update problemSet4.cpp

--- Graphs/problemSet4.cpp
+++ Graphs/problemSet4.cpp
@@ -126,3 +126,108 @@ public:
         return safe;
     }
 };
+
+// Code Written By me , BFS Logic Thats it
+// 1091. Shortest Path in Binary Matrix
+// Given an n x n binary matrix grid, return the length of the shortest clear path in the matrix. If there is no clear path, return -1.
+// A clear path in a binary matrix is a path from the top-left cell (i.e., (0, 0)) to the bottom-right cell (i.e., (n - 1, n - 1))
+class Solution {
+public: 
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        if(grid[0][0] == 1 || grid[n-1][n-1] == 1)return -1;
+        int res = grid[n-1][n-1];
+        vector< unordered_map < int , bool > > visited(n);
+        queue< tuple < int , int, int > > q;
+        vector< pair<int,int> > arr(8);
+        arr = { {-1,-1},{-1,0},{-1,1}, {0,-1},{0,1},{1,-1},{1,0},{1,1} };
+        q.push({0,0,1});
+        visited[0][0] = true;
+        while(!q.empty()){
+            auto top = q.front(); q.pop();
+
+            int i = get<0>(top);
+            int j = get<1>(top);
+            int c = get<2>(top);
+
+            if(i == n-1 && j == n-1) return c;
+            // for all directions
+            for(int d = 0 ; d < 8 ; d++){
+                int t1 = i + arr[d].first;
+                int t2 = j + arr[d].second;
+                if(t1 < n && t2 < n && t1 >= 0 && t2 >= 0 && !visited[t1][t2]){
+                    visited[t1][t2] = true;
+                    if(grid[t1][t2] == 0){
+                        q.push({t1,t2,c+1});
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+    
+};
+
+// CodeStudio 
+// // Shortest path in an unweighted graph
+// Problem statement
+// The city of Ninjaland is analogous to the unweighted graph. The city has ‘N’ houses numbered from 1 to ‘N’ respectively and are connected by M bidirectional
+// roads. If a road is connecting two houses ‘X’
+// and ‘Y’ which means you can go from ‘X’ to ‘Y’ or ‘Y’ to ‘X’. It is guaranteed that you can reach any house from any other house via some combination of roads. 
+// Two houses are directly connected by at max one road.
+// A path between house ‘S’ to house ‘T’ is defined as a sequence of vertices from ‘S’ to ‘T’. Where starting house is ‘S’ and the ending house is ‘T’ and there is 
+// a road connecting two consecutive houses. Basically, the path looks like this: (S , h1 , h2 , h3 , ... T). you have to find the shortest path from ‘S’ to ‘T’.
+// For example
+// In the below map of Ninjaland let say you want to go from S=1 to T=8, the shortest path is (1, 3, 8). You can also go from S=1 to T=8  via (1, 2, 5, 8)  
+//     or (1, 4, 6, 7, 8) but these paths are not shortest.
+
+vector<int> shortestPath( vector<pair<int,int>> edges , int n , int m, int s , int t){
+	// create adjcency list;
+	vector<vector<int> > adj(n);
+	for(int i = 0; i < m;i++){
+		int u = edges[i].first - 1;
+		int v = edges[i].second - 1;
+
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+
+	// perform bfs
+	queue<int> q;
+	vector<bool> visited(n);
+	vector<int> parent(n);
+	s--; t--;
+
+
+	q.push(s);
+	parent[s] = -1;
+	visited[s] = true;
+
+	while(!q.empty()){
+		int top = q.front();
+		q.pop();
+
+		for(int j : adj[top]){
+			// ans is locked here
+			if(!visited[j]){
+				visited[j] = true;
+				parent[j] = top;
+				q.push(j);
+			}
+		}
+	}
+
+
+	// parent array;
+	vector<int> res;
+	res.push_back(t + 1);
+	int p = parent[t];
+	while(p != -1){
+		res.push_back(p + 1);
+		p = parent[p];
+	}
+	reverse(res.begin(),res.end());
+	return res;
+}
+
+
